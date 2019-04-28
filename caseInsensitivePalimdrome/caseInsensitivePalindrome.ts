c0ddf0036f4a4364bc10e456a429cac92476638b
Richard <RichardGTech1990@gmail.com>
2019-04-28 12:53:18 -0700

    Did 30+ algorithms already???

---
+++ caseInsensitivePalimdrome/caseInsensitivePalindrome.ts
@@ -0,0 +1,9 @@
+function isCaseInsensitivePalindrome(inputString: string): boolean {
+    const originalLowerCase = inputString.toLowerCase();
+    const reversedWord = inputString.toLowerCase().split('').reverse().join('');
+
+    return originalLowerCase === reversedWord;
+}
+
+console.log(isCaseInsensitivePalindrome('AaBaa'));
+console.log(isCaseInsensitivePalindrome('abac'));
